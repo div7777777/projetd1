986763d591884affd5108da8e3746736eeb307dd
div7777777 <99870561+div7777777@users.noreply.github.com>
2022-10-05 19:37:40 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+//   menu pied de page
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
